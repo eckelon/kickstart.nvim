33e2297d94ee740b2ba4c90037687d6174708180
Jesus Samitier <contacto@jasamitier.com>
2023-02-18 01:20:09 +0100

    new config from kickstart.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "klen/nvim-test",
+		name = "nvim-test",
+		ft="python",
+		config = function()
+			require('nvim-test').setup {}
+		end
+	},
+	require 'kickstart.plugins.autoformat',
+	require 'kickstart.plugins.debug',
+}
